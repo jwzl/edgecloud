eaec80fbeb8aae3d0f4ba3d2a22e33fc5f896433
Qing <cnj1990@sina.com.cn>
2020-03-12 18:49:31 +0800

    add edgecloud

--- pkg/devicetwin/dtcontext.go
+++ pkg/devicetwin/dtcontext.go
@@ -51,3 +51,150 @@ func (dtc *DTContext) GetEdgeMutex (edgeID string) (*sync.Mutex, bool) {
 
 	return mutex, true
 }
+
+//Lock twin by ID
+func (dtc *DTContext) Lock (edgeID string) bool {
+	mutex, ok := dtc.GetEdgeMutex(edgeID)
+	if ok {
+		mutex.Lock()
+		return true
+	}
+
+	return false
+}
+
+//unlock twin by ID 
+func (dtc *DTContext) Unlock (edgeID string) bool {
+	mutex, ok := dtc.GetEdgeMutex(edgeID)
+	if ok {
+		mutex.Unlock()
+		return true
+	}
+
+	return false
+}
+
+/*
+* GetEdgeInfo get the edge information.
+*/
+func (dtc *DTContext) GetEdgeInfo(edgeID string) (*EdgeDescription, error){
+	v, exist := dtc.EdgeMap.Load(edgeID)
+	if !exist {
+		return nil, errors.New("edge is not exists.")
+	}
+
+	edged, isEdged := v.(*EdgeDescription)
+	if !isEdged {
+		return nil,  errors.New("edge is not exists.")
+	}
+
+	return edged, nil
+} 
+
+/*
+* AddEdgeInfo: Add edge information.
+*/
+func (dtc *DTContext) AddEdgeInfo(edged *EdgeDescription) error {
+	edgeID : = edged.ID
+	if err := dtc.GetEdgeInfo(edgeID); err == nil {
+		return errors.New("edge is exists.")
+	}
+
+	var edgeMutex	sync.Mutex
+	dtc.EdgeMutex.Store(edgeID, &edgeMutex)
+	dtc.EdgeMap.Store(edgeID, edged)
+
+	return nil
+}
+
+/*
+* Set edge State
+*/
+func (dtc *DTContext) SetEdgeState(edgeID, state string) error {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+	
+	edged.SetEdgeState(state)
+
+	return nil
+}
+
+/*
+* RegisterTwins: register the twin. 
+*/
+func (dtc *DTContext) RegisterTwins(edgeID, twinID string) error {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+
+	return edged.RegisterTwins(twinID)
+}
+
+/*
+* UnRegisterTwins: Unregister the twin. 
+*/
+func (dtc *DTContext) UnRegisterTwins(edgeID, twinID string) error {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+
+	return edged.UnRegisterTwins(twinID)
+}
+
+/*
+* UpdateTwin update twin in this edge
+*/
+func (dtc *DTContext) UpdateTwin(edgeID string, twin *common.DigitalTwin) error {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+
+	return edged.UpdateTwin(twin)
+}
+
+/*
+* DeleteTwin delete the twin.
+*/
+func (dtc *DTContext) DeleteTwin(edgeID, twinID string) error {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+
+	return edged.DeleteTwin(twinID)
+}
+
+/*
+* GetRawTwin: get the raw twin. 
+*/
+func (dtc *DTContext) GetRawTwin(edgeID, twinID string) ([]byte, error) {
+	dtc.Lock(edgeID)
+	defer dtc.Unlock(edgeID)
+
+	edged, err := dtc.GetEdgeInfo(edgeID)
+	if err != nil {
+		return err
+	}
+
+	return edged.GetRawTwin(twinID)
+}
--- pkg/devicetwin/edge.go
+++ pkg/devicetwin/edge.go
@@ -70,6 +70,38 @@ func (ed *EdgeDescription) FindTwins(twinID string) bool {
 	return false
 }
 
+func (ed *EdgeDescription) RegisterTwins(twinID string) error {
+	if ed.FindTwins(twinID) != true {
+		ed.deviceIDs = append(ed.deviceIDs, twinID)
+		return nil
+	}
+
+	return errors.New("twin is exists.")
+}
+
+func (ed *EdgeDescription) UnRegisterTwins(twinID string) error {
+	if ed.FindTwins(twinID) != true {
+		ed.deviceIDs = append(ed.deviceIDs, twinID)
+		return errors.New("twin is not exists.")
+	}
+
+	//delete(ed.deviceIDs, twinID)
+}
+
+func (ed *EdgeDescription) getTwin (twinID string) (*common.DigitalTwin, bool) {
+	v, exist := ed.Twins.Load(twinID)
+	if !exist {
+		return nil, false
+	}
+
+	twin, isTwin := v.(*common.DigitalTwin)
+	if !isTwin {
+		return nil, false
+	}
+
+	return twin, true
+}
+
 //getTwinMutex get the twin mutex
 func (ed *EdgeDescription) getTwinMutex (twinID string) (*sync.Mutex, bool) {
 	v, exist := ed.TwinMutex.Load(twinID)
@@ -108,7 +140,10 @@ func (ed *EdgeDescription) Unlock (twinID string) bool {
 }
 
 
-func (ed *EdgeDescription) UpdateTwin(twin common.DigitalTwin) error {
+/*
+* Update twin.
+*/
+func (ed *EdgeDescription) UpdateTwin(twin *common.DigitalTwin) error {
 	twinID := twin.ID
 	if ed.FindTwins(twinID) != true {
 		return errors.New("No such this twin")
@@ -117,11 +152,99 @@ func (ed *EdgeDescription) UpdateTwin(twin common.DigitalTwin) error {
 	_ , exist := ed.TwinMutex.Load(twinID)
 	if !exist {
 		//Create twin
-		
+		var deviceMutex	sync.Mutex
+		ed.TwinMutex.Store(twinID, &deviceMutex)
+
+		ed.Twins.Store(twinID, twin)
 	} else {
 		// update twin
 		ed.Lock(twinID)
 		defer ed.Unlock(twinID)	
+	
+		oldTwin, ok := ed.getTwin(twinID) 
+		if !ok {
+			return 
+		}
+		//update the twin
+		if len(twin.Name) > 0 {
+			oldTwin.Name = twin.Name
+		}
+		if len(twin.Description) > 0 {
+			oldTwin.Description = twin.Description
+		}
+		if len(twin.State) > 0 {
+			oldTwin.LastState = twin.State 
+			oldTwin.State = twin.State		
+		}
+
+		//patch all metadata to oldTwin.
+		if oldTwin.MetaData == nil {
+			oldTwin.MetaData = make(map[string]*common.MetaType)
+		} 
+		if len(twin.MetaData) > 0 {
+			for _ , meta := range twin.MetaData {
+				if meta != nil {
+					oldTwin.MetaData[meta.Name] = meta
+				}
+			}
+		}
+
+		//update desired
+		if len(twin.Properties.Desired) > 0 {
+			if oldTwin.Properties.Desired == nil {
+				oldTwin.Properties.Desired = make(map[string]*common.TwinProperty)
+			}
+
+			for _ , prop := range twin.Properties.Desired {
+				oldTwin.Properties.Desired[prop.Name] = prop
+			}
+		}	
+
+		//update reported
+		if len(twin.Properties.Reported) > 0 {
+			if oldTwin.Properties.Reported == nil {
+				oldTwin.Properties.Reported = make(map[string]*common.TwinProperty)
+			}
+
+			for _ , prop := range twin.Properties.Reported {
+				oldTwin.Properties.Desired[prop.Name] = prop
+			}
+		}	
 
 	} 
+}
+
+/*
+* Delete twin.
+*/
+func (ed *EdgeDescription) DeleteTwin(twinID string) error {
+	if ed.FindTwins(twinID) != true {
+		return errors.New("twin is not exists.")
+	}
+
+	ed.Lock(twinID)
+	ed.Twins.Delete(twinID)
+	ed.Unlock(twinID)
+	ed.TwinMutex.Delete(twinID)
+
+	return nil
+} 
+
+/*
+* Get Twin
+*/
+func (ed *EdgeDescription) GetRawTwin(twinID string) ([]byte, error) {
+	if ed.FindTwins(twinID) != true {
+		return nil, errors.New("twin is not exists.")
+	}
+
+	ed.Lock(twinID)
+	defer ed.Unlock(twinID)
+
+	twin, ok := ed.getTwin(twinID) 
+	if !ok {
+		return nil, errors.New("twin is not exists.") 
+	}
+
+	return json.Marshal(twin)
 }
