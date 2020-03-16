af352f2074a1a170e272f30c4ea8d452b8bf18c5
Qing <cnj1990@sina.com.cn>
2020-03-16 17:19:05 +0800

    fix sync

--- pkg/devicetwin/devicetwin.go
+++ pkg/devicetwin/devicetwin.go
@@ -2,8 +2,9 @@ package devicetwin
 
 
 type DeviceTwinModule struct {
-	context 	*context.Context
-	dtcontext 	*DTContext
+	DownStreamMessageChan	chan *model.Message
+	context 				*context.Context
+	dtcontext 				*DTContext
 }
 
 // Register this module.
@@ -26,9 +27,10 @@ func (dtm *DeviceTwinModule) Group() string {
 func (dtm *DeviceTwinModule) Start(c *context.Context) {
 
 	dtm.context = c
+	dtm.dtcontext = NewDTContext(c)	
 
 	for {
-		v, err := c.Receive(dtm.Name())
+		v, err := dtm.context.Receive(dtm.Name())
 		if err != nil {
 			klog.Errorf("failed to receive message: %v", err)
 			break
@@ -39,16 +41,63 @@ func (dtm *DeviceTwinModule) Start(c *context.Context) {
 			continue
 		}
 
-		operation := msg.GetOperation()
-		switch operation {
-		case common.DGTWINS_OPS_RESPONSE:
-		
-		case common.DGTWINS_OPS_SYNC:	
-		}	
+		target := msg.GetTarget()
+
+		if strings.Contains(target, common.CloudName) {
+			// Do Up stream message.
+			go dtm.doUpStreamMessage(msg)
+		}else if strings.Contains(target, common.TwinModuleName) {
+			// Do Down stream message.
+			go dtm.doDownStreamMessage(msg)
+		}
 	}
+	
+	
 }
 
 //Cleanup
 func (dtm *DeviceTwinModule) Cleanup() {
 	dtm.context.Cleanup(dtm.Name())
+}
+
+/*
+* doUpStreamMessage
+*/
+func (dtm *DeviceTwinModule) doUpStreamMessage(msg *model.Message) {
+	operation := msg.GetOperation()
+	resource  := msg.GetResource()
+	splitString := strings.Split(resource, "/")	
+	edgeID := splitString[0]
+
+	switch operation {
+	case common.DGTWINS_OPS_RESPONSE:
+		
+	case common.DGTWINS_OPS_SYNC:
+		twinMsg, err := common.UnMarshalTwinMessage(msg)
+		if err != nil {
+			return
+		}	
+
+		dgTwin := &twinMsg.Twins[0]
+
+		err := dtm.dtcontext.UpdateTwin(edgeID, dgTwin)
+		if err != nil {
+			return
+		}	
+		klog.Infof("Update successful.") 
+	}	
+}
+
+/*
+* doDownStreamMessage
+*/
+func (dtm *DeviceTwinModule) doDownStreamMessage(msg *model.Message) {
+	operation := msg.GetOperation()
+
+	switch operation {
+	case common.DGTWINS_OPS_RESPONSE:
+		
+	case common.DGTWINS_OPS_SYNC:
+				
+	}	
 }
